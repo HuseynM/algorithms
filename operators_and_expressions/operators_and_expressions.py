fd721a245546f68a6df2e2b6711f13277e3accb4
Huseyn <huseyn.mikayil95@gmail.com>
2018-07-26 05:37:48 -0700

    check_third_digit and check_third_bit function added

--- operators_and_expressions/operators_and_expressions.py
+++ operators_and_expressions/operators_and_expressions.py
@@ -12,11 +12,33 @@ def is_odd_or_even(n):
 def is_divisible_both_divisor(n, divisor1, divisor2):
     '''
     Write a Boolean expression that checks whether a given integer is
-                divisible by both divisor1 and divisor2, without a remainder.
+    divisible by both divisor1 and divisor2, without a remainder.
     :param n:
     :param divisor1:
     :param divisor2:
     :return:
     '''
     return n % divisor1 == 0 and n % divisor2 == 0
+
+def check_third_digit(n):
+    '''
+    Write an expression that looks for a given integer if its third digit (right
+    to left) is 7.
+    1570
+    :param n:
+    :return:
+    '''
+    return (n//100) % 10 == 7
+
+
+def check_third_bit(n):
+    '''
+    Write an expression that checks whether the third bit in a given integer
+    is 1 or 0.
+    :param n:
+    :return:
+    ..0100 = 4
+    '''
+    return 0 if (n & 0B100) == 0 else 1
+
 
