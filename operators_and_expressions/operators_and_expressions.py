28abd0efa975fd6a332fe39603db2445f90cb535
Huseyn <huseyn.mikayil95@gmail.com>
2018-07-26 06:15:54 -0700

    reverse function added

--- operators_and_expressions/operators_and_expressions.py
+++ operators_and_expressions/operators_and_expressions.py
@@ -56,3 +56,18 @@ def sum_of_digits(n):
 
     return sum
 
+
+def reverse_number(n):
+    '''
+    Prints on the console the number in reversed order: dcba
+    :param n:
+    :return:
+    '''
+
+    reversed = 0
+    while n > 0:
+        reversed *= 10
+        reversed += n % 10
+        n = n // 10
+    return reversed
+
