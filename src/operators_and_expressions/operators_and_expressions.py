3a3b9d8ed550992cc901f08a6a86f07e3cad32ff
Huseyn <huseyn.mikayil95@gmail.com>
2018-07-30 01:01:33 -0700

    change_bit_position added

--- src/operators_and_expressions/operators_and_expressions.py
+++ src/operators_and_expressions/operators_and_expressions.py
@@ -113,7 +113,7 @@ def exchange_digits(n):
     return n
 
 
-def check_p_bit_position_n(n, p):
+def print_bit_position(n, p):
     '''
     We are given a number n and a position p. Write a sequence of
     operations that prints the value of the bit on the position p in the
@@ -124,6 +124,44 @@ def check_p_bit_position_n(n, p):
     :return:
     '''
 
-    bit = n & (1 << (p-1))
+    bit = n & (1 << (p - 1))
     return 0 if bit == 0 else 1
+
+
+def check_bit_position(n, p):
+    '''
+    Write a Boolean expression that checks if the bit on position p in the
+    integer v has the value 1. Example v=5, p=1 -> false.
+    :param n:
+    :param p:
+    :return:
+    '''
+
+    bit = n & (1 << (p - 1))
+    return False if bit == 0 else True
+
+
+def change_bit_position(n, v, p):
+    '''
+    We are given the number n, the value v (v = 0 or 1) and the position p.
+    write a sequence of operations that changes the value of n, so the bit on
+    the position p has the value of v. Example: n=35, p=5, v=0 -> n=3.
+    Another example: n=35, p=2, v=1 -> n=39.
+    :param n:
+    :param v:
+    :param p:
+    :return:
+    '''
+
+    return n | (1 << (p - 1)) if v == 1 else n & ~(1 << (p - 1))
+
+
+
+
+
+
+
+
+
+
 
