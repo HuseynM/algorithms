737ec9b524dbbd2c8fde40a6d3bf4822cd1e13c3
Huseyn <huseyn.mikayil95@gmail.com>
2018-07-30 22:51:08 -0700

    find_biggest_from_three added

--- src/conditional_statements/conditional_statements.py
+++ src/conditional_statements/conditional_statements.py
@@ -16,3 +16,21 @@ def exchange_values(a, b):
 
     return nums
 
+
+def find_biggest_from_three(a, b, c):
+    '''
+    Write a program that finds the biggest of three integers, using nested
+    if statements.
+    :param a:
+    :param b:
+    :param c:
+    :return:
+    '''
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    elif c > a and c > b:
+        return c
+    else:
+        return 0
