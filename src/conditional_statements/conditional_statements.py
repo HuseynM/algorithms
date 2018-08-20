a70a21a5647b74a071af5e9073029b4d99d1394e
Huseyn <huseyn.mikayil95@gmail.com>
2018-08-20 04:48:03 -0700

    greatest and smallest finder added

--- src/conditional_statements/conditional_statements.py
+++ src/conditional_statements/conditional_statements.py
@@ -34,3 +34,32 @@ def find_biggest_from_three(a, b, c):
         return c
     else:
         return 0
+
+
+def find_greatest(a):
+    '''
+    Write a program that finds the greatest of given array.
+    :param a:
+    :return:
+    '''
+
+    _local = 0
+    for i in a:
+        if _local < i:
+            _local = i
+
+    return _local
+
+
+def find_smallest(a):
+    '''
+    Write a program that finds the smallest of given array.
+    :param a:
+    :return:
+    '''
+    _local = 0
+    for i in a:
+        if _local > i:
+            _local = i
+
+    return _local
